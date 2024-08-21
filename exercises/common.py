97cfd3b87ed1e226f55d3c757f19a073140a5872
Agustin Garassino <ajgarassino@gmail.com>
2024-08-21 09:35:19 -0300

    Add necessary common code

--- exercises/common.py
+++ exercises/common.py
@@ -36,3 +36,55 @@ class Sha3_256Transcript(Transcript):
         self.hasher = sha3_256()
         self.hasher.update(result)
         return result
+
+
+class Domain:
+    def __init__(self, omega: int):
+        """Produces the set of all powers of `omega` modulo `p` and stores them in `self._elements`"""
+        omega = F(omega)
+        size = omega.multiplicative_order()
+        self._elements = [omega ** i for i in range(size)]
+    
+    @classmethod
+    def of_size(cls, size: int):
+        """Returns a domain of size `size`."""
+        # generator of the full units group of 𝔽. That is, the powers 
+        # of `generator` produce all nonzero elements of 𝔽
+        generator = F.multiplicative_generator()
+        p = F.order()
+        if size <= 0 or (p - 1) % size != 0:
+            raise ValueError
+        omega = int(generator ** ((p - 1) // size))
+        return cls(omega)
+    
+    def __len__(self):
+        return len(self._elements)
+    
+    def __getitem__(self, index):
+        return self._elements[index]
+
+
+class Oracle:
+    def __init__(self, polynomial: Polynomial):
+        raise NotImplementedError("subclass responsibility")
+        
+    def query(self, z):
+        raise NotImplementedError("subclass responsibility")
+
+import sys
+
+class NaiveOracle(Oracle):
+    def __init__(self, polynomial: Polynomial):
+        self._polynomial = polynomial
+    
+    def query(self, z):
+        """
+        One-time single use function. Returns the value of the polynomial at `z`.
+        On first use this function dumps the polynomial and returns `None`
+        for subsequent calls.
+        """
+        if self._polynomial is not None:
+            y = self._polynomial(z)
+            self._polynomial = None
+            return y
+
